8ee60429fd545b235b99b6de087e0ee37261b671
abhi.t.a1806@gmail.com <abhi.t.a1806@gmail.com>
2026-05-31 01:47:38 +0530

    Initial project structure

---
+++ Backend/src/main/java/com/abhi/skillloopai/SkillloopAiApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.skillloopai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillloopAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillloopAiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/abhi/skillloopai/SkillloopAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.skillloopai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillloopAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
